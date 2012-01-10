76b566a1f2cb7937a8fbec3c706c229253409d1d
MAEDA Go <maeda@farend.jp>
2012-01-10 11:21:13 +0900

    update plugin attributes.

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_issue_filter_by_description do
   name 'Redmine Issue Filter By Description plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'FAR END Technologies Corporation'
+  description ''
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/farend/redmine_issue_filter_by_description'
+  author_url 'http://www.farend.co.jp/'
 end
