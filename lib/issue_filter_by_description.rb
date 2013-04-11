75f991ba3d2cde22ddb2e32fb0c3a85f70474d48
MAEDA Go <maeda@farend.jp>
2013-04-11 17:30:17 +0900

    Redmine 2.3 compatibility.
    - Obsolated the feature to display the issue descriptions. Same feature
    is supported by Redmine 2.2.
    - Fix AddDescriptionToAvailableFilters::InstanceMethods.available_filters_with_description to work with Redmine 2.3.

--- lib/issue_filter_by_description.rb
+++ lib/issue_filter_by_description.rb
@@ -5,7 +5,6 @@ module AddDescriptionToAvailableFilters
     base.send(:include, InstanceMethods)
     base.class_eval do
       alias_method_chain :available_filters, :description
-      alias_method_chain :available_columns, :description
     end
   end
 
@@ -13,40 +12,11 @@ module AddDescriptionToAvailableFilters
     def available_filters_with_description
       unless @available_filters
         available_filters_without_description
-        @available_filters["description"] = { :type => :text, :order => 99 }
+        @available_filters["description"] = { :type => :text, :order => 99, :name => l(:field_description)}
       end
       @available_filters
-    end
-  end
-  
-  def available_columns_with_description
-    unless @available_columns
-      available_columns_without_description
-      @available_columns << QueryColumn.new(:description, :sortable => "#{Issue.table_name}.description")
-    end
-    @available_columns
-  end
-end
-
-module SimpleFormatDescriptionColumn
-  def self.included(base)
-    base.module_eval do
-      base.send(:include, ModuleMethods)
-      alias_method_chain :column_content, :simple_format
-    end
-  end
-  
-  module ModuleMethods
-    def column_content_with_simple_format(column, issue)
-      if column.name == :description
-        value = column.value(issue)
-        simple_format(value)
-      else
-        column_content_without_simple_format(column, issue)
-      end
     end
   end
 end
 
 Query.send(:include, AddDescriptionToAvailableFilters)
-QueriesHelper.send(:include, SimpleFormatDescriptionColumn)
